09372271d415911dc0409b28e48b99929c29fd0a
Mari Alcántara <85371190+ma-al-1405@users.noreply.github.com>
2025-02-04 22:01:24 -0600

    se añade ejercicio-1 de la semana 2 a la carpeta

--- 2_samana/MAAAA-Flask_crud_app/crud_js_react/index.js
+++ 2_samana/MAAAA-Flask_crud_app/crud_js_react/index.js
@@ -1,1 +1,15 @@
+// backend/index.js
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
